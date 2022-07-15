9d4dc462e80dd12ef149fa486b0dc5017452f55b
JHerrera5 <jherrera@existaya.com>
2022-07-15 17:22:41 -0500

    Curso angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// import {LoggerService } from './logger.service';
+import { GreetingsService} from './greetings.service'
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public myName:string = 'Juan';
+  public titleStyles:{};
+  // public counter = 0;
+
+  constructor(private greetings:GreetingsService){
+  // constructor(private logger:LoggerService){
+
+  }
+  onSayHello(message){
+    // this.logger.log(message);
+    // console.log(message);
+
+    // this.counter++;
+    this.updateTitleStyles();
+    this.greetings.handleGreetings(message);
+  }
+
+  updateNameClasses(name:string){
+    return {
+      'error' : name.length <=3,
+      'warning' : name.length >3 && name.length <=6,
+      'success' : name.length >6,
+      'bold': name.length >8
+    }
+  }
+
+  updateTitleStyles(){
+    this.titleStyles = {
+      'margin-top': '40px',
+      // 'color': this.counter < 3 ? 'green' : 'goldenrod'
+      'color': this.greetings.counter < 3 ? 'green' : 'goldenrod'
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import {HelloComponent} from './hello/hello.component';
+import {UpperCapitalPipe} from './hello/upper-capital.pipe';
+//import {LoggerService} from './logger.service';
+import {GreetingsService} from './greetings.service';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    UpperCapitalPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  // providers: [LoggerService, GreetingsService],
+  providers: [GreetingsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
